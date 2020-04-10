f58bf0c4f1a5346c25fe7cde5314944c9e4c921d
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-04-10 17:43:22 -0400

    backend users can register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 const { check, validationResult } = require('express-validator'); //from documentation on github
 
+const User = require('../../models/Users');
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
@@ -16,7 +19,7 @@ router.post('/', [
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-(req, res) => {
+async (req, res) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -24,8 +27,55 @@ router.post('/', [
     }
 
 
+    const {name, email, password } = req.body
+
+    try{
+    //see if user exists
+        let user = await User.findOne({email });
+            if(user){
+                return res.status(400).json({erros: [ {msg: 'User already exists'}]})
+            }
+
+    // Get users gravatar
+        const avatar = 
+            gravatar.url(email, {
+            s: '200', //default size
+            r: 'pg', // rating
+            d: 'mm' //default img
+            })
+            ;
+        
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+    // Encrypt Passward using bcyptjs
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password,salt);
+
+        await user.save();
+        //everything that return a promise must put await infront of it.
+
+
+    //Return json-web-token
+
+
     // console.log(req.body);
-    res.send('User route')
+    res.send('User registered')
+        
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+    
 });
 
 module.exports = router;
+
+
+
+// diastataxy@vielove.com
