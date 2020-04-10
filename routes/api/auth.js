cde978ba0c44f303b6d36b2445f9a9c9d6fe5ccd
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-04-10 18:33:12 -0400

    middleware/jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,24 @@
 const express = require('express');
 
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/Users');
 
 // @route    POST api/auth
 // @desc     Register user
 // @access   Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async(req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+
+    }
+}
+);
 
 module.exports = router;
