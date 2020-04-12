b30999994d945dbce305f9dba612a88b9bd6d34a
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-04-11 20:30:02 -0400

    can add profile exp

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -182,6 +182,65 @@ router.get('/me', auth, async (req, res) => {
       })
 
 
+      // @route   PUT API/PROFILE/EXPERIENCE
+      // @desc    Add profile experience
+      // @access   Private
+
+      router.put('/experience', [auth,
+        [
+          check('title', 'Title is required').not().isEmpty(),
+          check('company', 'company is required').not().isEmpty(),
+          check('from', 'from data is required').not().isEmpty(),
+
+
+
+        ]
+      
+      ], 
+      async (req, res) =>{
+        const errors = validationResult(req);
+          if (!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+          }
+
+          //destruture
+          const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+          } = req.body;
+
+          const newExp = {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+          }
+
+          //now with mongoDB
+          try{
+            const profile = await Profile.findOne({ user: req.user.id});
+
+            profile.experience.unshift(newExp);
+            await profile.save();
+            res.json(profile);
+
+          } catch(err){
+            console.error(err.message);
+            res.status(500).send('Server Error')
+          }
+       
+
+      });
+
+
 
 
 
