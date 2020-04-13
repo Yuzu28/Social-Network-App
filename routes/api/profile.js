71a4262b13ad7ab833af7d3124107d4bb7b8203d
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-04-12 23:08:26 -0400

    backend users posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth')
+const auth = require('../../middleware/auth');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/Users');
