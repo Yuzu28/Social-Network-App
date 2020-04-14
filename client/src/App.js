e2495706d29aa1edf1377e65a0a87d501ebc9b04
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-04-14 00:28:12 -0400

    added login/register componet

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import React, { Fragment } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+
 
 const  App = () => (
+  <Router>
   
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+      <Fragment>
+        {/* <Navbar /> */}
+        <Route  exact path="/" component={Landing}/>
+        <Router exact path="/register" component={Register} />
+
+        <section className="container">
+          <Switch>
+            <Router exact path="/register" component={Register} />
+            <Router exact path="/login" component={Login} />
+
+          </Switch>
+
+
+        </section>
+      </Fragment>
+  </Router>
   
 )
 
