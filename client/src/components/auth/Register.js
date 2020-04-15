f23538be3344910a2179fa04c87014ea2a58014a
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-04-14 23:22:45 -0400

    redux store

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -100,36 +100,3 @@ export default Register;
 
 
 
-
-
-{/* another way to connect to the backend through testing different methods */}
-
-    {/* const onSubmit = async e => {
-        e.preventDefault();
-        if(password !== password2)  {
-            console.log('Passwords do Not Match')
-
-        }else{
-            const newUser = {
-                name,
-                email,
-                password
-            }
-            try{
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-                const body = JSON.stringify(newUser);
-
-                //connect to the backend
-                const res = await axios.post('/api/users', body, config);
-                console.log(res.data);
-
-            }catch (err){
-                console.error(err.response.data);
-
-            }
-        }
-    } */}
---
+++ client/src/store.js
@@ -0,0 +1,18 @@
+//creating a redux store
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+//our middleware
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    rootReducer, 
+    initialState, 
+    composeWithDevTools(applyMiddleware(...middleware))
+    );
+
+export default store;
