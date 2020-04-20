8bbe615948954b235ecfdb40bf4e81c68f91d44a
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-04-20 19:58:28 -0400

    Post reducer, action, and component

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -22,6 +22,10 @@ export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 
+export const GET_POSTS = 'GET_POSTS';
+export const POST_ERROR = 'POST_ERROR';
+
+
 
 
 
