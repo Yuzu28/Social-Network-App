f6901eab324bccb8e055da441402643ceeac9c40
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-04-20 23:27:46 -0400

    deleting posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,5 +1,5 @@
 //post reducer
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -18,6 +18,12 @@ export default function(state = initialState, action) {
         posts: payload,
         loading: false
       };
+    case DELETE_POST:
+      return{
+        ...state,
+        posts: state.posts.filter(post => post._id !== payload),
+        loading: false
+      }
     case POST_ERROR:
       return {
         ...state,
