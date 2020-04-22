2cfeb23b372febc67b43337e3b7d75b87b1011ea
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-04-21 23:21:54 -0400

    comment display and delete

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -33,6 +33,11 @@ export const ADD_POST = 'ADD_POST';
 
 export const GET_POST = 'GET_POST';
 
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+
+
 
 
 
